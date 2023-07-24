c60029ad2421186fb7a6ad21e71f8db39a868cd3
vsevolod <vsevolod.smrnv@gmail.com>
2023-07-24 16:40:07 +0300

    add Route announcement for learning app

--- lms_project/learning/views.py
+++ lms_project/learning/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def create(request):
+    return HttpResponse('Страница создания нового курса')
+
+
+def delete(request, course_id):
+    return HttpResponse(f'Страница для удаления {course_id} курса')
+
+
+def detail(request, course_id):
+    return HttpResponse(f'Страница информации о {course_id} курсе')
+
+
+def enroll(request, course_id):
+    return HttpResponse(f'Страница для записи на курс {course_id}')
+
+
+def index(request):
+    return HttpResponse('Страница отображения всех доступных курсов')
