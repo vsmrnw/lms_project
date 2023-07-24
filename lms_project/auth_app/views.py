0b08eb701caeaffdee19b082d68c23ee83deb2e3
vsevolod <vsevolod.smrnv@gmail.com>
2023-07-24 15:05:41 +0300

    add Route announcement for auth_app

--- lms_project/auth_app/views.py
+++ lms_project/auth_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def login(request):
+    return HttpResponse('Страница для входа пользователя на сайт')
+
+
+def register(request):
+    return HttpResponse('Страница для регистрации пользователя')
+
+
+def logout(request):
+    return HttpResponse('Выход и перенаправление на страницу входа')
+
+
+def change_password(request):
+    return HttpResponse('Обработчик для смены пароля пользователя')
+
+
+def reset_password(request):
+    return HttpResponse('В обработчике реализована логика сброса пароля пользователя')
