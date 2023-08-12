4eda28f34f4666da68ac6f84c12742118ea3022e
vsevolod <vsevolod.smrnv@gmail.com>
2023-08-12 14:20:33 +0300

    add course review feature

--- lms_project/auth_app/urls.py
+++ lms_project/auth_app/urls.py
@@ -10,11 +10,15 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='user_login'), name='logout'),
     # Обработчики для смены пароля
     path('password_change/', PasswordChangeView.as_view(), name='password_change'),
-    path('password_change/done', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_change/done', PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
     # Обработчики сброса пароля
     path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
-    path('password-reset/done', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password-reset/done', PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>', PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset/done', PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 
 ]
